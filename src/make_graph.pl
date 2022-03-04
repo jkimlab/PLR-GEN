e4604e77c989998a2a219bc6c085adc3fb385804
jkimlab <bioinfolab.kr@gmail.com>
2022-03-04 14:27:59 +0900

    Update make_graph.pl

--- src/make_graph.pl
+++ src/make_graph.pl
@@ -9,20 +9,24 @@ my $min_length = $ARGV[1];
 my $min_cnt = $ARGV[2];
 my $out_dir = $ARGV[-1];
 
-#my $min_length = 1;
+#---------------------------------------------------------------------
+###### Preprocessing 1 : extracting aligned columns with alignment depth >= min_cutoff
 $min_length = $min_length/10;
-#my $min_cnt = 1;
 `mkdir -p $out_dir`;
 `awk '\$4>=$min_cnt {print \$1 \"\t\" \$2-1 \"\t\" \$2 \"\t\" \$3 \"\t\" \$4 \"\t\" \$5 \"\t\" \$6 \"\t\" \$7}' $in_mpileup > $out_dir/mapped.bed`;
 if(-z "$out_dir/mapped.bed"){
 		`touch $out_dir/.no_mapping`;
 		exit();
 }
+#---------------------------------------------------------------------
+###### Preprocessing 2 : creating PLR containers
 `cut -f1,2,3 $out_dir/mapped.bed > $out_dir/pos.bed`; 
 `$Bin/../bin/bedtools sort -i $out_dir/pos.bed > $out_dir/sort.pos.bed`;
 `$Bin/../bin/bedtools merge -i $out_dir/sort.pos.bed | awk '\$3-\$2>=$min_length' - > $out_dir/container_list.bed`;
 `$Bin/../bin/bedtools intersect -wo -a $out_dir/container_list.bed -b $out_dir/mapped.bed > $out_dir/mapping_info_in_container.txt`;
 
+#---------------------------------------------------------------------
+###### Making PLR graphs
 open(FOGRAPH, ">$out_dir/graph.out");
 open(FCONTAINER, "$out_dir/mapping_info_in_container.txt");
 my %hs_list = ();
@@ -30,13 +34,15 @@ my $saved_node = "";
 my $pre_container = "";
 my $coord = 0;
 my $bubble_id = 0;
-while(<FCONTAINER>){
+
+######### converting alignment columns to nodes of PLR graphs
+while(<FCONTAINER>){ # for each base position
 		chomp;
+## parsing information of an alignment 
 		$coord += 1;
 		my ($c1, $c2, $c3, $m1, $t1, $pos, $r_base, $count, $aln, $mapq, $reads, $t2) = split(/\t/,$_);
 		my $container_name = "$c1:$c2-$c3";
 		if($pre_container ne $container_name){
-#				print ">$container_name\n";
 				$pre_container = $container_name;
 				$coord = 1;
 				$bubble_id=0;
@@ -44,11 +50,11 @@ while(<FCONTAINER>){
 		my $processed_aln = $aln;
 		$processed_aln =~ tr/,acgt/.ACGT/;
 		my @aln = split(//,$processed_aln);
-##		print STDERR "$processed_aln\n";
 		my @g_node = ();
 		my ($cur_base, $next_base) = ("", "");
 		for(my $i = 0; $i <= $#aln; $i++){
-## alignment start and end processing
+
+## parsing aligned bases
 				if($aln[$i] eq "^"){
 						$i += 1; #skip next character (mapping quality)
 						next;
@@ -76,10 +82,8 @@ while(<FCONTAINER>){
 										last;
 								}
 						}
-##						print STDERR "$extracting_start\t$insert_size\t";
 						$i = $extracting_start+$insert_size-1;
 						my $insert_base = substr($processed_aln, $extracting_start, $insert_size);
-##						print STDERR "$insert_base\n";
 						if($next_base eq "+"){
 								push(@g_node, "$cur_base$insert_base");
 						}
@@ -90,12 +94,13 @@ while(<FCONTAINER>){
 		}
 		my $depth = scalar(@g_node);
 		if($depth < $min_cnt){ next; }
+
+## counting mapping depth for each base (A, C, G, and T)
 		my %hs_cnt = ();
 		my %hs_read = ();
 		my @read = split(/,/,$reads);
 		for(my $i = 0; $i <= $#g_node; $i++){
 				my $node = $g_node[$i];
-##				print STDERR "$node\n";
 				if(!exists($hs_cnt{$node})){
 						$hs_cnt{$node} = 1; 
 						$hs_read{$node} = $read[$i];
@@ -105,9 +110,10 @@ while(<FCONTAINER>){
 						$hs_read{$node} .= ",$read[$i]";
 				}
 		}
+
+## creating normal and bubble nodes
 		my @count = sort {$b<=>$a} values(%hs_cnt);
 		my $s_coord = $coord-1;
-#		if($count[0] == scalar(@g_node) || $count[0] == (scalar(@g_node)-1)){
 		if($count[0] == scalar(@g_node)){
 				print FOGRAPH "NORMAL\t$container_name\t$s_coord\t$coord\t$g_node[0]\n";
 		}
@@ -118,8 +124,10 @@ while(<FCONTAINER>){
 						my $mapped_read = join(",",@reads);
 						$bubble_id += 1;
 						print FOGRAPH "BUBBLE\t$container_name\t$s_coord\t$coord\t$b_node\t$hs_cnt{$b_node}\t$mapped_read\n";
-#						print FOGRAPH "BUBBLE$bubble_id\t$container_name\t$s_coord\t$coord\t$b_node\t$hs_cnt{$b_node}\t$mapped_read\n";
 				}
 		}
 }
 close(FCONTAINER);
+
+#---------------------------------------------------------------------
+
