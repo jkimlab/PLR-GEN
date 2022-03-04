1bd58d3ed701d966693e1bfb305633f89fbfec4a
jkimlab <bioinfolab.kr@gmail.com>
2022-03-04 13:49:12 +0900

    Update ref_preparation_TAMA.pl

--- src/ref_preparation_TAMA.pl
+++ src/ref_preparation_TAMA.pl
@@ -16,7 +16,6 @@ my $out_dir = "./ref_dir";
 my $in_cpu = 1;
 my $help;
 
-
 ## parameters
 my $options = GetOptions(
 		"s=s" => \$read_s, # mandatory (incompatible with -1 and -2)
@@ -41,6 +40,9 @@ if(defined($read2) && !defined($read1)){
 }
 
 `mkdir -p $out_dir`;
+
+#---------------------------------------------------------------------
+###### Creating a TAMA input parameter file
 open(FPR, ">$out_dir/params");
 PRINT_PARAM1();
 if(defined($read_s)){
@@ -56,8 +58,12 @@ else{
 PRINT_PARAM2();
 close(FPR);
 
+#---------------------------------------------------------------------
+###### Running TAMA
 `$Bin/../bin/TAMA/TAMA.pl -p $in_cpu -o $out_dir/tamaout --param $out_dir/params`;
 my %hs_taxid = ();
+#---------------------------------------------------------------------
+###### Storing taxonomy IDs of predicted species
 open(FAB, "$out_dir/tamaout/TEST/sample1/abundance_profile.0.34.out");
 while(<FAB>){
 		chomp;
@@ -67,6 +73,8 @@ while(<FAB>){
 		$hs_taxid{$t[1]} = 1;
 }
 close(FAB);
+#---------------------------------------------------------------------
+###### Downloading reference genomes with "Chromosome" and "Complete Genome" level from NCBI
 `mkdir -p $out_dir/ref_fa`;
 open(FTXT, "gunzip -c $Bin/../sources/assembly_summary.txt.gz |");
 while(<FTXT>){
@@ -83,7 +91,12 @@ while(<FTXT>){
 		}
 }
 close(FTXT);
+#---------------------------------------------------------------------
+###### Finishing
 `ls $out_dir/ref_fa/* > $out_dir/ref_list.txt`;
+#---------------------------------------------------------------------
+
+###### Printing the first part of the parameter file of TAMA
 sub PRINT_PARAM1{
 		print FPR "[Project]\n";
 		print FPR "\$PROJECTNAME=TEST\n\n";
@@ -99,12 +112,16 @@ sub PRINT_PARAM1{
 		print FPR "[Input]\n";
 		print FPR ">sample1\n";
 }
+
+#---------------------------------------------------------------------
+###### Printing the last part of the parameter file of TAMA
 sub PRINT_PARAM2{
 		print FPR "[Preprocessing]\n";
 		print FPR "\$TRIMMOMATIC-RUN=true\n";
 		print FPR "\$TRIMMOMATIC-OPTION=\n";
 		print FPR "\$BAYESHAMMER-RUN=true\n\n";
 }
+#---------------------------------------------------------------------
 
 sub HELP{
 		my $src = basename($0);
